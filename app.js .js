48d159bd2272fc426f80752767db97b01ea3ea1e
N Weah <nw206@student.le.ac.uk>
2026-05-10 19:15:33 +0100

    starting the javascript - creating the task data

---
+++ app.js .js
@@ -0,0 +1,14 @@
+// This file handles all intereactions 
+//This being rendering tasks and stats 
+// Kanban darg and drop 
+// Adding new tasks through modal 
+// adding new tasks through the modal 
+// risk register rendering 
+
+// I'm storing all task dtaa in this data array so everything 
+// re renders from one source of truth whenever something changes 
+
+const tasks = [ 
+    { id:1, name:" Define product requirements & scope", owner: "Natasha W", initials: "nw", color: "#58a6ff", priority: "high", 
+        deadline:"10-02-2026", status: "done" }, 
+]
